16a30b7066d253f99b829b6427d3cfe513fae36f
Shwe Yee Winn <yonngelay@gmail.com>
2020-05-18 12:40:39 +0800

    added getMetricMetaInfo function in helpers.js

--- utiles/helpers.js
+++ utiles/helpers.js
@@ -45,29 +45,81 @@ export function timeToString(time = Date.now()) {
   return todayUTC.toISOString().split('T')[0]
 }
 
-export function getMetricMetaInfo() {
+export function getMetricMetaInfo(metric) {
+
   const info = {
     run: {
-      displayName: 'Run',
+      displayName: "Run",
       max: 50,
-      unit: 'miles',
+      unit: "miles",
       step: 1,
-      type: 'steppers',
+      type: "steppers",
       getIcon() {
-        return ()
+        return (
+          <View>
+            <MaterialIcons name="directions-run" color={"black"} size={35} />
+          </View>
+        );
       }
     },
     bike: {
-
+      displayName: "Bike",
+      max: 100,
+      unit: "miles",
+      step: 1,
+      type: "steppers",
+      getIcon() {
+        return (
+          <View>
+            <MaterialCommunityIcons name="bike" color={white} size={32} />
+          </View>
+        );
+      }
     },
     swim: {
-
+      displayName: "Swim",
+      max: 9900,
+      unit: "meters",
+      step: 100,
+      type: "steppers",
+      getIcon() {
+        return (
+          <View>
+            <MaterialCommunityIcons name="swim" color={white} size={35} />
+          </View>
+        );
+      }
     },
     sleep: {
-
+      displayName: "Sleep",
+      max: 24,
+      unit: "hours",
+      step: 1,
+      type: "slider",
+      getIcon() {
+        return (
+          <View>
+            <FontAwesome name="bed" color={white} size={30} />
+          </View>
+        );
+      }
     },
     eat: {
-
+      displayName: "Eat",
+      max: 10,
+      unit: "rating",
+      step: 1,
+      type: "slider",
+      getIcon() {
+        return (
+          <View>
+            <MaterialCommunityIcons name="food" color={white} size={35} />
+          </View>
+        );
+      }
     }
-  }
+  };
+
+  return typeof metric === "undefined" ? info : info[metric];
+
 }
