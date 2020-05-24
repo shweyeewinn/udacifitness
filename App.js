443c66d58d6e550e248a8fe0ca0fd2df06936c14
Shwe Yee Winn <yonngelay@gmail.com>
2020-05-24 14:24:09 +0800

    CreateStore

--- App.js
+++ App.js
@@ -10,33 +10,18 @@ import {
 } from 'react-native'
 
 import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 export default class App extends React.Component {
-
-  handlePress = () => {
-    alert('Hello')
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <AddEntry />
-        {/* <TouchableHighlight style={styles.btn} onPress={this.handlePress} underlayColor='#000'>
-          <Text style={styles.btnText}>Touchable Highlight</Text>
-        </TouchableHighlight> */}
-        {/* <TouchableOpacity style={styles.btn} onPress={this.handlePress}>
-          <Text style={styles.btnText}>Touchable Opacity</Text>
-        </TouchableOpacity> */}
-        {/* <TouchableWithoutFeedback onPress={this.handlePress}>
-          <View style={styles.btn}><Text style={styles.btnText}>Touchable Without Feedback</Text></View>
-        </TouchableWithoutFeedback> */}
-        {/* Only supported for Android */}
-        {/* <TouchableNativeFeedback
-          background={TouchableNativeFeedback.SelectableBackground}
-          onPress={this.handlePress}>
-          <View style={styles.btn}><Text style={styles.btnText}>TouchableNativeFeedback</Text></View>
-        </TouchableNativeFeedback> */}
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={styles.container}>
+          <AddEntry />
+        </View>
+      </Provider>
     );
   }
 
