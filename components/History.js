6fe1a2772e1ed405106143ba1177b4f67ef812fc
Shwe Yee Winn <yonngelay@gmail.com>
2020-05-27 14:40:14 +0800

    udacifitness-calendar

--- components/History.js
+++ components/History.js
@@ -3,7 +3,10 @@ import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { receiveEntries, addEntry } from '../actions';
 import { timeToString, getDailyReminderValue } from '../utils/helpers';
-import { fetchCalendarResults } from '../utils/api'
+import { fetchCalendarResults } from '../utils/api';
+import UdaciFitnessCalendar from 'udacifitness-calendar';
+
+
 
 class History extends Component {
   componentDidMount() {
@@ -21,14 +24,31 @@ class History extends Component {
         }
       })
   }
-
-  render() {
+  renderItem = ({ today, ...metrics }, formattedDate, key) => (
+    <View>
+      {
+        today
+          ? <Text>{JSON.stringify(today)}</Text>
+          : <Text>{JSON.stringify(metrics)}</Text>
+      }
+    </View>
+  )
+  renderEmptyDate(formattedDate) {
     return (
       <View>
-        <Text>
-          {JSON.stringify(this.props)}
-        </Text>
+        <Text>No data for this day.</Text>
       </View>
+    )
+  }
+  render() {
+    const { entries } = this.props;
+
+    return (
+      <UdaciFitnessCalendar
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
     )
   }
 }
